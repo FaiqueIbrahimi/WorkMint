7ec9f2f217b4081735da3ebf446ebea7af7d8951
Faique <faique.official695@gmail.com>
2025-07-27 05:38:59 +0530

    Initial frontend setup with Navbar in process

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from "./components/shared/Navbar"
+
+ 
+function App() {
+  return (
+    <>
+    <h1 className="text-red-300">hehe boi</h1>
+    <Navbar></Navbar>
+    
+    </>
+  )
+}
+ 
+export default App
