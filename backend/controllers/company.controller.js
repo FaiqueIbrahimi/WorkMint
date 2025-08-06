733adad0c415eb6892a8975d71bba92509165d39
Faique <faique.official695@gmail.com>
2025-08-06 17:14:32 +0530

    Add file upload functionality to company registration and update hooks

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     
@@ -67,11 +69,11 @@ export const updateCompany = async (req, res) => {
     
         const { name, description, website, location } = req.body;
  
-        const file = req.file;
+        const file = req.file;  
         // // idhar cloudinary ayega
-        // const fileUri = getDataUri(file);
-        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        // const logo = cloudResponse.secure_url;
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
     
         const updateData = { name, description, website, location };
 
